1ee414f8cdadb4632578cef68980d7a1d463393d
Sahstiva <avitshas@gmail.com>
2021-12-12 12:21:01 +0300

    Реализован материал второго урока

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -5,26 +5,50 @@ import {
   Post,
   Body,
   Delete,
-  Put, HttpException, HttpStatus,
+  Put,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { News, NewsEdit, NewsService } from './news.service';
+import { renderNewsAll } from '../views/news/news-all';
+import { renderTemplate } from '../views/template';
+import { CommentsService } from './comments/comments.service';
 
 @Controller('news')
 export class NewsController {
-  constructor(private readonly newsService: NewsService) {}
+  constructor(
+    private readonly newsService: NewsService,
+    private readonly commentsService: CommentsService,
+  ) {}
 
-  @Get('/detail/:id')
+  @Get('/api/detail/:id')
   get(@Param('id') id: string): News {
     const idInt = parseInt(id);
-    return this.newsService.find(idInt);
+    const news = this.newsService.find(idInt);
+    const comments = this.commentsService.find(idInt);
+
+    return {
+      ...news,
+      comments,
+    };
   }
 
-  @Get('/all')
+  @Get('/api/all')
   getAll(): News[] {
     return this.newsService.getAll();
   }
 
-  @Put('/:id')
+  @Get('/all')
+  getAllView() {
+    const news = this.newsService.getAll();
+    const content = renderNewsAll(news);
+    return renderTemplate(content, {
+      title: 'Список новостей',
+      description: 'Самый крутые новости на свете!',
+    });
+  }
+
+  @Put('/api/:id')
   edit(@Param('id') id: string, @Body() news: NewsEdit): News {
     const idInt = parseInt(id);
     const result = this.newsService.edit(idInt, news);
@@ -39,12 +63,12 @@ export class NewsController {
       );
   }
 
-  @Post()
+  @Post('/api')
   create(@Body() news: News): News {
     return this.newsService.create(news);
   }
 
-  @Delete('/:id')
+  @Delete('/api/:id')
   remove(@Param('id') id: string): string {
     const idInt = parseInt(id);
     const isRemoved = this.newsService.remove(idInt);
--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Comment } from './comments/comments.service';
 
 export interface News {
   id?: number;
@@ -6,6 +7,8 @@ export interface News {
   description: string;
   author: string;
   countView?: number;
+  cover?: string;
+  comments?: Comment[];
 }
 
 export interface NewsEdit {
@@ -14,9 +17,10 @@ export interface NewsEdit {
   description?: string;
   author?: string;
   countView?: number;
+  cover?: string;
 }
 
-function getRandomInt(min: number, max: number): number {
+export function getRandomInt(min = 1, max = 9999): number {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
@@ -31,6 +35,8 @@ export class NewsService {
       description: 'Ураааа! Наша первая новость',
       author: 'Владислав',
       countView: 12,
+      cover:
+        'https://cdnn21.img.ria.ru/images/148839/96/1488399659_0:0:960:960_600x0_80_0_1_e38b72053fffa5d3d7e82d2fe116f0b3.jpg',
     },
   ];
 
@@ -62,8 +68,9 @@ export class NewsService {
       };
       return this.news[indexEditNews];
     }
-    return this.news.find((news) => news.id === id);
+    return undefined;
   }
+
   remove(id: News['id']): boolean {
     const indexRemoveNews = this.news.findIndex((news) => news.id === id);
     if (indexRemoveNews !== -1) {
